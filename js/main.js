87cc2ae42184eb00e2d36de4fa55058764ed14e9
devorx <derevyankoolx@gmail.com>
2022-09-07 16:46:29 +0300

    adaptive first page

--- js/main.js
+++ js/main.js
@@ -3,12 +3,19 @@ $(function () {
     $('.banner-section__slider').slick({
         dots: true,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt="arrow"></button>',
-        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt="arrow"></button>'
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt="arrow"></button>',
+        responsive: [
+            {
+                breakpoint: 969,
+                settings: {
+                    arrows: false,
+                }
+            }
+        ]
     });
 
 
-    // рабочие табы //
-    $('.tab').on('click', function (e) {
+    $('.tab').on('click', function(e){
         e.preventDefault();
 
         $($(this).siblings()).removeClass('tab--active');
@@ -17,8 +24,14 @@ $(function () {
         $(this).addClass('tab--active');
         $($(this).attr('href')).addClass('tabs-content--active');
 
-    });
+        
+        // $('.tab').removeClass('tab--active');
+        // $('.tabs-content').removeClass('tabs-content--active');
 
+        // $(this).addClass('tab--active');
+        // $($(this).attr('href')).addClass('tabs-content--active');
+
+    });
 
     $('.product-item__favorite').on('click', function () {
         $(this).toggleClass('product-item__favorite--active');
@@ -29,11 +42,50 @@ $(function () {
             slidesToShow: 4,
             slidesToScroll: 1,
             prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt="arrow"></button>',
-            nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt="arrow"></button>'
+            nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt="arrow"></button>',
+            responsive: [
+                {
+                    breakpoint: 1301,
+                    settings: {
+                        arrows: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 1201,
+                    settings: {
+                        slidesToShow: 3,
+                        arrows: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 1101,
+                    settings: {
+                        slidesToShow: 3,
+                        arrows: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 870,
+                    settings: {
+                        slidesToShow: 2,
+                        arrows: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 590,
+                    settings: {
+                        slidesToShow: 1,
+                        arrows: false,
+                        dots: true
+                    }
+                }
+            ]
         });
     });
-
-
 
 
 
@@ -67,7 +119,7 @@ $(function () {
         prefix: "$"
     });
 
-    $('.catalog__filter-btngrid').on('click', function(){
+    $('.catalog__filter-btngrid').on('click', function () {
         $(this).addClass('catalog__filter-button--active');
         $('.catalog__filter-btnline').removeClass('catalog__filter-button--active');
         $('.product-item__wrapper').removeClass('product-item__wrapper--list');
@@ -75,7 +127,7 @@ $(function () {
 
 
 
-    $('.catalog__filter-btnline').on('click', function(){
+    $('.catalog__filter-btnline').on('click', function () {
         $(this).addClass('catalog__filter-button--active');
         $('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
         $('.product-item__wrapper').addClass('product-item__wrapper--list');
@@ -85,10 +137,18 @@ $(function () {
         normalFill: "#c4c4c4",
         ratedFill: "#1c62cd",
         spacing: "7px"
-      });
-
-
-    $('.menu__btn').on('click', function() {
-        $('.menu-mobile__list').toggleClass('menu-mobile__list--active')
     });
+
+
+    $('.menu__btn').on('click', function () {
+        $('.menu-mobile__list').toggleClass('menu-mobile__list--active');
+    });
+
+
+    $('.footer__top-drop').on('click', function (){
+        $(this).next().slideToggle();
+        $(this).toggleClass('footer__top-drop--active')
+    });
+
 });
+
